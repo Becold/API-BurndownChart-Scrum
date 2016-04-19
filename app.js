9f15b5d6be0c0b0c1245cce90d3e7a1bfb33530c
Rominou <befire@hotmail.fr>
2016-04-19 15:58:33 +0200

    Better middlewares

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ var express = require('express'),
 // Cfg
 app.use(bodyparser.urlencoded({"extended" : false}));
 app.use(bodyparser.json());
-app.use('/', middlewares);
+app.use(middlewares.allowCrossDomain);
+app.use(middlewares.onRequest);
 app.use('/', router);
 
 // Init
 db = require('./app/lib/database').init();
-app.listen(3000);
+app.listen(3000, function() {
+    console.log("[API] L'api ecoute sur le port 3000.");
+});
 
--- app/routes/middlewares.js
+++ app/routes/middlewares.js
@@ -1,9 +1,18 @@
-var middlewares = require('express').Router(),
-    util = require('util');
+var util = require('util');
 
-middlewares.use(function(req, res, next) {
-    console.log("[API] Request " + req.method + " " + req.url + " : " + util.inspect(req.body) );
-    next();
-});
+middlewares = {
+    onRequest: function(req, res, next) {
+        console.log("[API] Request " + req.method + " " + req.url + " : " + util.inspect(req.body) );
+
+        next();
+    },
+    allowCrossDomain: function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', 'http://localhost:63342');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+        next();
+    }
+};
 
 module.exports = middlewares;
